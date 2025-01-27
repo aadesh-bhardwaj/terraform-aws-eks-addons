8eb5eaa743fad224ba30cb22bcda941fbfb71c77
Aadesh Bhardwaj <122770344+aadesh-bhardwaj@users.noreply.github.com>
2025-01-27 23:47:38 +0530

    Update 7.17.7

--- modules/argocd/variables.tf
+++ modules/argocd/variables.tf
@@ -14,7 +14,7 @@ variable "argocd_config" {
 
 variable "chart_version" {
   type        = string
-  default     = "7.3.11"
+  default     = "7.17.7"
   description = "Version of the Argocd chart that will be used to deploy Argocd application."
 }
 
